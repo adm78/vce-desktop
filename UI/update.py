8067c5dd17bd4bcc9a1945ecc4c0262739e9fde6
Andrew D. McGuire <amcguire227@gmail.com>
2016-08-10 22:35:43 +0100

    More cross platform fixing for the UI updater

--- UI/update.py
+++ UI/update.py
@@ -2,7 +2,14 @@
 
 '''This script can bed used to update GUI .py and .pyr files after
 editing the .ui files and .qrc files (resource files) on Qt
-Designer. Any new ui widgets require a line to be added in this file''' 
+Designer. Any new ui widgets require a line to be added in this file. 
+
+for some reason this doesn't play nice cross-platform, hence why its
+ugly handling for Windows...On linux we are able to check that the
+executable is in the path, doesn't seem to work with pyuic on Windows,
+even if the .exe eXtension is added...
+
+''' 
 
 import subprocess 
 import os
@@ -10,24 +17,24 @@ from utils import which, isWindows
 import sys
 
 #update the main window .py
+if not isWindows():
+    #checking for the correct executables 
+    pyuic_exec = "pyuic"
+    pyuic4_exec = "pyuic4"
 
-#checking for the correct executables 
-pyuic_exec = "pyuic"
-pyuic4_exec = "pyuic4"
-if isWindows():
-    pyuic_exec += ".exe"
-    pyuic4_exec += ".exe"
+    if which(pyuic_exec) != None:
+        pyuic_cmd = "pyuic vceMainWindowGUI.ui > vceMainWindowGUI.py"
 
-if which(pyuic_exec) != None:
-    pyuic_cmd = "pyuic vceMainWindowGUI.ui > vceMainWindowGUI.py"
-    
-elif which(pyuic4_exec) != None:
-    pyuic_cmd = "pyuic4 vceMainWindowGUI.ui > vceMainWindowGUI.py"
+    elif which(pyuic4_exec) != None:
+        pyuic_cmd = "pyuic4 vceMainWindowGUI.ui > vceMainWindowGUI.py"
+    else:
+        print "update.py Error: neither pyuic or pyuic4 could not be found."
+        print "Try installing pyqt4-dev-tools or adding pyuic to your"
+        print "path and re-running."
+        sys.exit()
+
 else:
-    print "update.py Error: neither pyuic or pyuic4 could not be found."
-    print "Try installing pyqt4-dev-tools or adding pyuic to your"
-    print "path and re-running."
-    sys.exit()
+    pyuic_cmd = "pyuic vceMainWindowGUI.ui > vceMainWindowGUI.py"
 
 pyuic_process = subprocess.Popen(pyuic_cmd,
                                  stdout=subprocess.PIPE,
@@ -42,17 +49,25 @@ if errorcode != 0:
     print "exit code:", errorcode
     
 #updating the GUI resource file
-if which("pyrcc4") != None:
-    pyrcc_cmd = "pyrcc4 " + os.path.join("GUIresources","GUIresources.qrc") + " > GUIresources_rc.py"
-    pyrcc4_process = subprocess.Popen(pyrcc_cmd,
-                     stdout=subprocess.PIPE,
-                     stderr=subprocess.PIPE,
-                     shell=True)
+if not isWindows():
+    if which("pyrcc4") != None:
+        pyrcc_cmd = ("pyrcc4 " + os.path.join("GUIresources","GUIresources.qrc") 
+                     + " > GUIresources_rc.py")
+
+    else:
+        print "update.py Error: pyrcc4 could not be found."
+        print "Try installing pyqt4-dev-tools or adding pyrcc4 to your"
+        print "path and re-running."   
+        sys.exit()
+
 else:
-    print "update.py Error: pyrcc4 could not be found."
-    print "Try installing pyqt4-dev-tools or adding pyrcc4 to your"
-    print "path and re-running."   
-    sys.exit()
+    pyrcc_cmd = ("pyrcc4 " + os.path.join("GUIresources","GUIresources.qrc") 
+                 + " > GUIresources_rc.py")
+
+pyrcc4_process = subprocess.Popen(pyrcc_cmd,
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE,
+                                  shell=True)
 
 out, err = pyrcc4_process.communicate()
 errorcode = pyrcc4_process.returncode
