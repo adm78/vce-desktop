6041baf4a03746aaec2b6b88271994eeb6ee18df
Andrew McGuire <amcguire227@gmail.com>
2016-08-10 21:47:39 +0100

    fixing the UI update script so that it works under Linux

--- UI/update.py
+++ UI/update.py
@@ -4,11 +4,50 @@
 editing the .ui files and .qrc files (resource files) on Qt
 Designer. Any new ui widgets require a line to be added in this file''' 
 
-import subprocess
+import subprocess 
 import os
+from utils import which
 
 #update the main window .py
-subprocess.call(["pyuic", "vceMainWindowGUI.ui", ">", "vceMainWindowGUI.py"], shell=True)
+if which("pyuic") != None:
+    pyuic_cmd = "pyuic vceMainWindowGUI.ui > vceMainWindowGUI.py"
+    
+elif which("pyuic4") != None:
+    pyuic_cmd = "pyuic4 vceMainWindowGUI.ui > vceMainWindowGUI.py"
+else:
+    print "update.py Error: neither pyuic or pyuic4 could not be found."
+    print "Try installing pyqt4-dev-tools or adding pyuic to your"
+    print "path and re-running."
+    sys.exit()
 
+pyuic_process = subprocess.Popen(pyuic_cmd,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE,
+                                 shell=True)
+
+out, err = pyuic_process.communicate()
+errorcode = pyuic_process.returncode
+if errorcode != 0:
+    print "stdout:", out
+    print "stderr:", err
+    print "exit code:", errorcode
+    
 #updating the GUI resource file
-subprocess.call(["pyrcc4", "./GUIresources/GUIresources.qrc", ">", "GUIresources_rc.py"], shell=True)
+if which("pyrcc4") != None:
+    pyrcc_cmd = "pyrcc4 " + os.path.join("GUIresources","GUIresources.qrc") + " > GUIresources_rc.py"
+    pyrcc4_process = subprocess.Popen(pyrcc_cmd,
+                     stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE,
+                     shell=True)
+else:
+    print "update.py Error: pyrcc4 could not be found."
+    print "Try installing pyqt4-dev-tools or adding pyrcc4 to your"
+    print "path and re-running."   
+    sys.exit()
+
+out, err = pyrcc4_process.communicate()
+errorcode = pyrcc4_process.returncode
+if errorcode != 0:
+    print "stdout:", out
+    print "stderr:", err
+    print "exit code:", errorcode
--- lib/utils.py
+++ lib/utils.py
@@ -28,4 +28,24 @@ def getVCEPropertiesPath():
     #returns the path of the properties database
     return os.path.join(getVCEPath(),'properties')
 
+def which(program):
+    
+    ''' Cross platform functional to check if an executable exists
+    http://stackoverflow.com/questions/
+    377017/test-if-executable-exists-in-python'''
+    
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
 
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
